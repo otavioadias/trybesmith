cd70c9590e3c1d31ab3727392af45142c397ae68
Otávio Azevedo <otavioad1997@gmail.com>
2022-10-06 16:10:17 -0300

    Requisito 2 - Crie um endpoint para a listagem de produtos

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -9,6 +9,11 @@ class ProductController {
     const newProduct = await this.service.create(product);
     res.status(201).json(newProduct);
   };
+
+  public getAll = async (req: Request, res: Response) => {
+    const products = await this.service.getAll();
+    res.status(200).json(products);
+  };
 }
 
 export default ProductController;
--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -6,5 +6,6 @@ const productController = new ProductController();
 const router = Router();
 
 router.post('/', productController.newProduct);
+router.get('/', productController.getAll);
 
 export default router;
